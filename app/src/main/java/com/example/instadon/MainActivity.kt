1cc050470ae1942590e43bc359f97ef5ddf5ef06
Srivathsan Venkateswaran <srivathsanvenkateswaran@gmail.com>
2021-02-14 18:22:30 +0530

    Enhanced Sign In Activity and Added Sign Out Feature

--- app/src/main/java/com/example/instadon/MainActivity.kt
+++ app/src/main/java/com/example/instadon/MainActivity.kt
@@ -1,23 +1,36 @@
 package com.example.instadon
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.instadon.daos.PostDao
 import com.example.instadon.models.Post
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.Query
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity(), IPostAdapter {
 
     private lateinit var adapter: PostAdapter
     private lateinit var postDao: PostDao
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        setSupportActionBar(toolbar)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
 
         addNewPostFloatingActionButton.setOnClickListener{
             val intent = Intent(this, AddNewPostActivity::class.java)
@@ -25,6 +38,29 @@ class MainActivity : AppCompatActivity(), IPostAdapter {
         }
 
         setUpRecyclerView()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.googleSignOutMenu -> googleSignOut()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun googleSignOut() {
+//        Implement the Logic to sign out from Google here.
+        Toast.makeText(this, "Signing out...", Toast.LENGTH_LONG).show()
+
+        auth = Firebase.auth
+        auth.signOut()
+
+        val intent = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
     }
 
     private fun setUpRecyclerView() {
